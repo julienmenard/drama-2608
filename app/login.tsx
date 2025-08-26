645d9d03e0d9343fe946bf1040e0067923de94e5
Julien <juli1.menard@gmail.com>
2025-08-27 00:35:25 +0200

    Updated login.tsx

--- app/login.tsx
+++ app/login.tsx
@@ -25,6 +25,8 @@ export default function LoginScreen() {
 
     if (success) {
       router.replace('/(tabs)');
+    } else {
+      Alert.alert(t('error'), t('invalidCredentials'));
     }
   };
 
